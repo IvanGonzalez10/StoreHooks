aea4ac5f7cc47b13769e4e88ae63ca06eb37d6d9
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2021-01-24 14:34:45 -0500

    adding styles

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import "./styles.scss";
+
 export const Characters = () => {
   const [characters, setCharacters] = useState([]);
 
@@ -12,7 +14,14 @@ export const Characters = () => {
   return (
     <div className="Characters">
       {characters.map((character) => (
-        <h2>{character.name}</h2>
+          <div className="card">
+            <h2>{character.name}</h2>
+            <img src={character.image} alt={character.name} />
+            <h3>Estado: {character.status}</h3>
+            <h3>Genero: {character.gender}</h3>
+            <h3>Especie: {character.species}</h3>
+            <h3>Location: {character.location.name}</h3>
+        </div>
       ))}
     </div>
   );
