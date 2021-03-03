c021a09c9b23ceec1fa6639e66f37b96f39c4f31
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2021-03-03 15:55:22 -0500

    starting with styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
 
+import '../styles/Layout.scss';
+
 
 export const Layout = ({ children }) => {
   return (
     <div className='main'>
       <Header />
-      {Children}
+      {children}
       <Footer />
     </div>
   )
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
-import {Home} from '../components/Home';
-import {Checkout} from '../components/Checkout';
-import {Information} from '../components/Information';
-import {Payment} from '../components/Payment';
-import {Success} from '../components/Success';
-import {NotFound} from '../components/NotFound';
-
-
+import { Home } from "../components/Home";
+import { Checkout } from "../components/Checkout";
+import { Information } from "../components/Information";
+import { Payment } from "../components/Payment";
+import { Success } from "../components/Success";
+import { NotFound } from "../components/NotFound";
+import { Layout } from "../components/Layout";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
-  )
-}
+  );
+};
