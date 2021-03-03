2f9dddd64c7f2e8ff8f1d4481a8118b7486bd3b1
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2021-03-03 17:28:20 -0500

    adding style of information section

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+import '../styles/Information.scss'
 
 export const Information = () => {
   return (
-    <h1>Information</h1>
+    <div className="information">
+      <div className="information-content">
+        <div className="information-head">
+          <h2>Informacion de contacto:</h2>
+          <div className="information-form">
+            <form action="">
+              <input type="text" placeholder="Nombre completo" name="name" />
+              <input
+                type="text"
+                placeholder="Correo electronico"
+                name="email"
+              />
+              <input type="text" placeholder="Direccion" name="address" />
+              <input type="text" placeholder="Apto" name="apto" />
+              <input type="text" placeholder="Ciudad" name="city" />
+              <input type="text" placeholder="Pais" name="country" />
+              <input type="text" placeholder="Departamento" name="state" />
+              <input type="text" placeholder="Codigo postal" name="cp" />
+              <input type="text" placeholder="Telefono" name="phone" />
+            </form>
+          </div>
+          <div className="information-buttons">
+            <div className="information-back">Regresar</div>
+            <div className="information-next">Pagar</div>
+          </div>
+        </div>
+      </div>
+      <div className="information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="information-item">
+          <div className="information-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
